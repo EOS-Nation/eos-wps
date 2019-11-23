eb9baf4b098916fbd6546c77335a87bb6005015c
Denis <carriere.denis@gmail.com>
2019-11-23 13:33:26 -0500

    re-order folder structure

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,36 +6,7 @@ CYAN='\033[1;36m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-NODEOS_HOST="127.0.0.1"
-NODEOS_PROTOCOL="http"
-NODEOS_PORT="8888"
-NODEOS_LOCATION="${NODEOS_PROTOCOL}://${NODEOS_HOST}:${NODEOS_PORT}"
-
-# temp keosd setup
-WALLET_DIR=/tmp/temp-eosio-wallet
-UNIX_SOCKET_ADDRESS=$WALLET_DIR/keosd.sock
-WALLET_URL=unix://$UNIX_SOCKET_ADDRESS
-
-function cleos() { command cleos --verbose --url=${NODEOS_LOCATION} --wallet-url=$WALLET_URL "$@"; echo $@; }
-on_exit(){
-  echo -e "${CYAN}cleaning up temporary keosd process & artifacts${NC}";
-  kill -9 $TEMP_KEOSD_PID &> /dev/null
-  rm -rf $WALLET_DIR
-}
-trap my_trap INT
-trap my_trap SIGINT
-
-# start temp keosd
-rm -rf $WALLET_DIR
-mkdir $WALLET_DIR
-keosd --wallet-dir=$WALLET_DIR --unix-socket-path=$UNIX_SOCKET_ADDRESS &> /dev/null &
-TEMP_KEOSD_PID=$!
-sleep 1
-
-# create temp wallet
-cleos wallet create --file /tmp/.temp_keosd_pwd
-PASSWORD=$(cat /tmp/.temp_keosd_pwd)
-cleos wallet unlock --password="$PASSWORD"
+# load default key into local wallet
 cleos wallet import --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
 
 # create accounts
@@ -52,12 +23,12 @@ cleos create account eosio mybp4 EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5
 cleos create account eosio mybp5 EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
 cleos set account permission eosio.wps active --add-code
 
-
 # deploy smart contract
 echo -e "${CYAN}-----------------------DEPLOYING CONTRACTS-----------------------${NC}"
 cleos set contract eosio.wps ./dist eosio.wps.wasm eosio.wps.abi
 cleos set contract eosio.token ./dist eosio.token.wasm eosio.token.abi
 
+# create token
 echo -e "${CYAN}-----------------------CREATE EOS TOKEN-----------------------${NC}"
 cleos push action eosio.token create '["eosio", "1000000000.0000 EOS"]' -p eosio.token
 cleos push action eosio.token issue '["eosio", "1000000000.0000 EOS", "init"]' -p eosio
